6c20c8699864a9cdf02c3538832c109b13321693
BlingSmile <30430100+BlingSmile1994@users.noreply.github.com>
2018-06-02 12:08:38 +0800

    Bquery DOM选择器

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #### welcome to BINUI
-> call me BINGO
+
+BINUI 为本人的一个自己的前端学习项目，将会借鉴许多其他的UI设计
---
+++ js/Bquery.js
@@ -0,0 +1,63 @@
+/*
+ * @Author: BlingBling 
+ * @Date: 2018-06-01 18:55:46 
+ * @Last Modified by: BlingBling
+ * @Last Modified time: 2018-06-02 12:07:06
+ * 原型链学习— --- DOM操作
+ * 模拟jquery的选择器
+ */
+
+(function (window, undefined) {
+    var Bquery = function (selecter) {
+        return new Bquery.fn.init(selecter);
+    }
+
+    /**
+     * 解析标签
+     */
+    Bquery.fn.prototype = {
+        init: (selecter, context) => {
+            if (!selecter) {
+                return this;
+            } else if (typeof selecter === "string") {
+                //id正则
+                idSelectorRE = /^#([\w-]*)$/
+                //class 正则
+                classSelectorRE = /^\.([\w-]+)$/
+                //DOM正则
+                tagSelectorRE = /^[\w-]+$/
+                var elem;
+                if (selecter.match(idSelectorRE)) {
+                    console.log("id选择器");
+                    var id = selecter.slice(1, );
+                    elem = document.getElementById(id);
+                    return elem;
+                }else if(selecter.match(classSelectorRE)){
+                    console.log("class选择器");
+                    var className = selecter.slice(1,);
+                    elem = document.getElementsByClassName(className);
+                    return elem;
+                }else if(selecter.match(tagSelectorRE)){
+                    console.log("DOM选择器");
+                    elem = document.getElementsByTagName(selecter);
+                    return elem;
+                }
+            } else if (selecter.nodeType) {
+                // HANDLE: $(DOMElement)
+                this.context = this[0] = selecter;
+                this.length = 1;
+                return this;
+            }
+        },
+        length: 0,
+        size: () => {
+            return length;
+        },
+        bquery: "0.1.1"
+    }
+
+    Bquery.fn.init.prototype = Bquery.fn;
+    window.Bquery = window.$ = Bquery
+
+})(window);
+
